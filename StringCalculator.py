bea69f6a0f6c8cb63f0c55f048f72b413d222728
iconicx27 <77856027+iconicx27@users.noreply.github.com>
2024-07-22 15:33:10 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,13 +1,23 @@
-delimiters = ["//", "," ,";" ,"\n","1001"]
+delimiters = ["//", "," ,";" ,"\n"]
 
-def remove_delimiters(input_string):
+def split_string(s):
     for delimiter in delimiters:
-        input_string = input_string.replace(delimiter, '')
-    return input_string
+        s = s.replace(delimiter, delimiters[0])
+    filtered_list = [s for s in string_list if int(s) <= 1000]
+    return s.split(delimiters[0])
+
+
+# def remove_delimiters(input_string):
+#     for delimiter in delimiters:
+#         input_string = input_string.replace(delimiter, '')
+
+#     input_string=input_string.
+    
+#     return input_string
 
 def add(numbers):
     if not numbers:
         return 0
     
-    num_list = remove_delimiters(numbers)
+    num_list = split_string(numbers)
     return sum(int(num) for num in num_list)
