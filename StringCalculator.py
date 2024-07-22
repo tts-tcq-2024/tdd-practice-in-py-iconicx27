9c2f13f43be6529b8b878b2d82dfa620d699e219
iconicx27 <77856027+iconicx27@users.noreply.github.com>
2024-07-22 10:17:28 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,3 +1,11 @@
 def add(numbers):
     if numbers == "":
         return 0
+    elif numbers == "0":
+        return 0
+    else:
+        num_list = numbers.split(",")
+        ans=0
+        for num in num_list:
+            ans+=int(num)
+        return ans
