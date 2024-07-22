cd9ef11c6ae8b338e602c21f01c867f0bb4c63da
iconicx27 <77856027+iconicx27@users.noreply.github.com>
2024-07-22 10:34:36 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,5 +1,13 @@
+delimiters = ["//", "," ,";" ,"\n"]
+
+def remove_delimiters(input_string):
+    for delimiter in delimiters:
+        input_string = input_string.replace(delimiter, '')
+    return input_string
+
 def add(numbers):
     if not numbers:
         return 0
-    num_list = numbers.split(",")
+    
+    num_list = remove_delimiters(numbers)
     return sum(int(num) for num in num_list)
