4a91fc4f705425b6a89ff088b45b32572b607ad5
iconicx27 <77856027+iconicx27@users.noreply.github.com>
2024-07-22 10:40:15 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,11 +1,8 @@
-delimiters = ["//", "," ,";" ,"\n"]
+delimiters = ["//", "," ,";" ,"\n","1001"]
 
 def remove_delimiters(input_string):
     for delimiter in delimiters:
         input_string = input_string.replace(delimiter, '')
-
-    input_string = input_string.replace("1001", "1")
-    
     return input_string
 
 def add(numbers):
